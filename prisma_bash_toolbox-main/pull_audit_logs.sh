df47eaa10ffb0fbedf26d53f363724903d1f7c3b
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-01-11 17:26:04 -0500

    Create pull_audit_logs.sh

---
+++ prisma_bash_toolbox-main/pull_audit_logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Requires jq to be installed
+# Author Kyle Butler
+
+
+source ./secrets/secrets
+
+# Time filter. Assign the appropriate values. This will pull the last month's worth of audit logs. 
+TIMEAMOUNT=1
+TIMEUNIT="month"
+TIMETYPE="relative"
+
+
+AUTH_PAYLOAD=$(cat <<EOF
+{"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
+EOF
+)
+
+
+PC_JWT=$(curl --request POST \
+              --url "$PC_APIURL/login" \
+              --header 'Accept: application/json; charset=UTF-8' \
+              --header 'Content-Type: application/json; charset=UTF-8' \
+              --data "${AUTH_PAYLOAD}" | jq -r '.token')
+
+
+# returns as json
+curl --request GET \
+     --url "$PC_APIURL/audit/redlock?timeType=$TIMETYPE&timeAmount=$TIMEAMOUNT&timeUnit=$TIMEUNIT" \
+     --header "x-redlock-auth: $PC_JWT" | jq
+
+
+exit

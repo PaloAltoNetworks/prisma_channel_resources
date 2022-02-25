3f9191b8fb053051762c5f27053315f309faba0a
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-02-25 13:26:08 -0500

    updated script
    added request debugging

--- prisma_bash_toolbox-main/pull_audit_logs.sh
+++ prisma_bash_toolbox-main/pull_audit_logs.sh
@@ -11,23 +11,39 @@ TIMEUNIT="month"
 TIMETYPE="relative"
 
 
+#### NO EDITS NEEDED BELOW
+
+
+function quick_check {
+  res=$?
+  if [ $res -eq 0 ]; then
+    echo "$1 request succeeded"
+  else
+    echo "$1 request failed error code: $res"
+    exit
+  fi
+}
+
 AUTH_PAYLOAD=$(cat <<EOF
 {"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
 EOF
 )
 
 
-PC_JWT=$(curl --request POST \
-              --url "$PC_APIURL/login" \
-              --header 'Accept: application/json; charset=UTF-8' \
-              --header 'Content-Type: application/json; charset=UTF-8' \
-              --data "${AUTH_PAYLOAD}" | jq -r '.token')
+PC_JWT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/login" \
+                       --header 'Accept: application/json; charset=UTF-8' \
+                       --header 'Content-Type: application/json; charset=UTF-8' \
+                       --data "${AUTH_PAYLOAD}")
 
+quick_check "/login"
+
+PC_JWT=$( printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
 
 # returns as json
 curl --request GET \
      --url "$PC_APIURL/audit/redlock?timeType=$TIMETYPE&timeAmount=$TIMEAMOUNT&timeUnit=$TIMEUNIT" \
      --header "x-redlock-auth: $PC_JWT" | jq
 
-
+quick_check "/audit/redlock?timeType=$TIMETYPE&timeAmount=$TIMEAMOUNT&timeUnit=$TIMEUNIT"
 exit

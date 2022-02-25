c2f1d009adba2e64a605f061fa76a90bb8f1b603
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-02-25 13:24:09 -0500

    updated script
    added request debugging

--- prisma_bash_toolbox-main/detailed_compliance_alert_report.sh
+++ prisma_bash_toolbox-main/detailed_compliance_alert_report.sh
@@ -25,46 +25,76 @@ TIME_UNIT="month"
 TIME_AMOUNT="1"
 STATUS="open"
 
+
+#### NO EDITS NEEDED BELOW
+
+function quick_check {
+  res=$?
+  if [ $res -eq 0 ]; then
+    echo "$1 request succeeded"
+  else
+    echo "$1 request failed error code: $res"
+    exit
+  fi
+}
+
 AUTH_PAYLOAD=$(cat <<EOF
 {"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
 EOF
 )
+
+
+PC_JWT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/login" \
+                       --header 'Accept: application/json; charset=UTF-8' \
+                       --header 'Content-Type: application/json; charset=UTF-8' \
+                       --data "${AUTH_PAYLOAD}")
+
+quick_check "/login"
+
+
+PC_JWT=$(printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
+
 REPORT_DATE=$(date  +%m_%d_%y)
 
-PC_JWT=$(curl --silent \
-              --request POST \
-              --url "$PC_APIURL/login" \
-              --header 'Accept: application/json; charset=UTF-8' \
-              --header 'Content-Type: application/json; charset=UTF-8' \
-              --data "$AUTH_PAYLOAD" | jq -r '.token')
+COMPLIANCE_ID_RESPONSE=$(curl --request GET \
+                              --url "$PC_APIURL/compliance" \
+                              --header "x-redlock-auth: $PC_JWT" )
+
+quick_check "/compliance"
+
+COMPLIANCE_ID=$(printf %s "$COMPLIANCE_ID_RESPONSE" | jq -r --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '.[] | select(.name == $COMPLIANCE_NAME) | .id')
 
 
-COMPLIANCE_ID=$(curl --request GET \
-                     --url "$PC_APIURL/compliance" \
-                     --header "x-redlock-auth: $PC_JWT" | jq -r --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '.[] | select(.name == $COMPLIANCE_NAME) | .id')
+SECTION_ID_RESPONSE=$(curl --request GET \
+                           --url "$PC_APIURL/compliance/{$COMPLIANCE_ID}/requirement" \
+                           --header "x-redlock-auth: $PC_JWT")
+
+quick_check "/compliance/{$COMPLIANCE_ID}/requirement"
+
+SECTION_ID=$(printf %s "$SECTION_ID_RESPONSE" | jq -r '.[].id')
 
 
-SECTION_ID=$(curl --request GET \
-                  --url "$PC_APIURL/compliance/{$COMPLIANCE_ID}/requirement" \
-                  --header "x-redlock-auth: $PC_JWT" | jq -r '.[].id')
 
 declare -a SECTION_ID_ARRAY=($(printf %s "$SECTION_ID"))
 
 COMPLIANCE_SECTION_RESPONSE=$(for SECTION_ID in "${SECTION_ID_ARRAY[@]}"; do
                                     curl --request GET \
-			                 --url $PC_APIURL/compliance/{$SECTION_ID}/section \
-			                 --header "x-redlock-auth: $PC_JWT" | jq '.[]';
-                              done)    
+                                         --url $PC_APIURL/compliance/{$SECTION_ID}/section \
+                                         --header "x-redlock-auth: $PC_JWT" | jq '.[]';
+                              done)
 
 
 POLICY_ID_ARRAY=($(printf %s "$COMPLIANCE_SECTION_RESPONSE" | jq -r '.associatedPolicyIds[]'))
 
 echo -e "standardName, requirementId, sectionId, account, resourceName, resourceType, url, tagKey, tagValue, policyName, policyDescription \n" >  ./detailed_compliance_alert_report_$REPORT_DATE.csv
 
-for POLICY_ID in ${POLICY_ID_ARRAY[@]}; do 
-	          curl --request GET \
-                       --url "$PC_APIURL/v2/alert?timeType=$TIME_TYPE&timeAmount=$TIME_AMOUNT&timeUnit=$TIME_UNIT&detailed=true&policy.id=$POLICY_ID&status=$STATUS" \
-		       --header "x-redlock-auth: $PC_JWT" | jq --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '.items[] | select(.status == "open") | {standardName: .policy.complianceMetadata[].standardName, requirementId: .policy.complianceMetadata[].requirementId, sectionId: .policy.complianceMetadata[].sectionId, account: .resource.account, resourceName: .resource.name, resourceType: .resource.resourceType, url: .resource.url, tagKey: .resource.data.tags[].key, tagValue: .resource.data.tags[].value, policyName: .policy.name, policyDescription: .policy.description} | select(.standardName == $COMPLIANCE_NAME)' | jq -r '[.] | map({standardName, requirementId, sectionId, account, resourceName, resourceType, url, tagKey, tagValue, policyName, policyDescription}) | (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows | $rows[] | @csv' >> ./detailed_compliance_alert_report_$REPORT_DATE.csv
+for POLICY_ID in ${POLICY_ID_ARRAY[@]}; do
+  POLICY_RESPONSE=$(curl --request GET \
+                         --url "$PC_APIURL/v2/alert?timeType=$TIME_TYPE&timeAmount=$TIME_AMOUNT&timeUnit=$TIME_UNIT&detailed=true&policy.id=$POLICY_ID&status=$STATUS" \
+                         --header "x-redlock-auth: $PC_JWT")
+  quick_check "/v2/alert?timeType=$TIME_TYPE&timeAmount=$TIME_AMOUNT&timeUnit=$TIME_UNIT&detailed=true&policy.id=$POLICY_ID&status=$STATUS"
+  printf %s "$POLICY_RESPONSE" | jq --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '.items[] | select(.status == "open") | {standardName: .policy.complianceMetadata[].standardName, requirementId: .policy.complianceMetadata[].requirementId, sectionId: .policy.complianceMetadata[].sectionId, account: .resource.account, resourceName: .resource.name, resourceType: .resource.resourceType, url: .resource.url, tagKey: .resource.data.tags[].key, tagValue: .resource.data.tags[].value, policyName: .policy.name, policyDescription: .policy.description} | select(.standardName == $COMPLIANCE_NAME)' | jq -r '[.] | map({standardName, requirementId, sectionId, account, resourceName, resourceType, url, tagKey, tagValue, policyName, policyDescription}) | (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows | $rows[] | @csv' >> ./detailed_compliance_alert_report_$REPORT_DATE.csv
 done 
 
 exit

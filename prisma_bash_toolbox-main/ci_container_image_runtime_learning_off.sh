19d71311ae74594ba61aed3e256d5f8b5213044a
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-02-25 13:15:30 -0500

    updated script
    added debugging to requests

--- prisma_bash_toolbox-main/ci_container_image_runtime_learning_off.sh
+++ prisma_bash_toolbox-main/ci_container_image_runtime_learning_off.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Written by Kyle Butler
 # Turns off the runtime workload learning process. For use during CI or during staging. 
 source ./secrets/secrets
 
-#Put container image tag here
+# Put container image tag here
 IMAGE_TAG="swaggerapi/petstore:latest"
+
+#### NO EDITS NEEDED BELOW
 
 LEARNING_PAYLOAD=$(cat <<EOF
 {"state":"manualActive"}
@@ -12,11 +14,26 @@ EOF
 )
 
 
+function quick_check {
+  res=$?
+  if [ $res -eq 0 ]; then
+    echo "$1 request succeeded"
+  else
+    echo "$1 request failed error code: $res"
+    exit
+  fi
+}
+
+
 # Retrieves the IMAGE PROFILE ID from the compute console
 # self-hosted may require curl -k depending on if you're using a self-signed cert
-PROFILE_ID=$(curl -X GET \
-                  -u $TL_USER:$TL_PASSWORD \
-                  --url "$TL_CONSOLE/api/v1/profiles/container?search=$IMAGE_TAG" | jq -r '.[]._id')
+PROFILE_ID_RESPONSE=$(curl -X GET \
+                           -u $TL_USER:$TL_PASSWORD \
+                           --url "$TL_CONSOLE/api/v1/profiles/container?search=$IMAGE_TAG")
+
+quick_check "/api/v1/profiles/container"
+
+PROFILE_ID=$(printf %s "$PROFILE_ID_RESPONSE" | jq -r '.[]._id')
 
 # Turns off the learning mode for the container image setting it to active. 
 # self-hosted may require curl -k depending on if you're using a self-signed cert
@@ -25,3 +42,5 @@ curl -X POST \
      --url "$TL_CONSOLE/api/v1/profiles/container/$PROFILE_ID/learn" \
      -H "Content-Type: application/json" \
      -d $LEARNING_PAYLOAD
+
+quick_check "/api/v1/profiles/container/$PROFILE_ID/learn"

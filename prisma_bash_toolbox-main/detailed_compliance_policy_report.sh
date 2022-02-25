79a1add2f7f048e44c6464aeff65126bbd50e90f
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-02-25 13:24:50 -0500

    updated script
    added request debugging

--- prisma_bash_toolbox-main/detailed_compliance_policy_report.sh
+++ prisma_bash_toolbox-main/detailed_compliance_policy_report.sh
@@ -9,24 +9,48 @@ source ./secrets/secrets
 COMPLIANCE_NAME="PCI DSS v3.2.1"
 
 
+
+
+
+#### NO EDITS NEEDED BELOW
+
+function quick_check {
+  res=$?
+  if [ $res -eq 0 ]; then
+    echo "$1 request succeeded"
+  else
+    echo "$1 request failed error code: $res"
+    exit
+  fi
+}
+
 AUTH_PAYLOAD=$(cat <<EOF
 {"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
 EOF
 )
+
+
+PC_JWT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/login" \
+                       --header 'Accept: application/json; charset=UTF-8' \
+                       --header 'Content-Type: application/json; charset=UTF-8' \
+                       --data "${AUTH_PAYLOAD}")
+
+quick_check "/login"
+
+
+PC_JWT=$(printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
+
 REPORT_DATE=$(date  +%m_%d_%y)
 POLICY_REPORT_NAME="./Policy_report_$REPORT_DATE.csv"
-PC_JWT=$(curl --silent \
-              --request POST \
-              --url "$PC_APIURL/login" \
-              --header 'Accept: application/json; charset=UTF-8' \
-              --header 'Content-Type: application/json; charset=UTF-8' \
-              --data "$AUTH_PAYLOAD" | jq -r '.token')
 
+POLICY_JSON_RESPONSE=$(curl --request GET \
+                            --url "$PC_APIURL/policy?" \
+                            --header "x-redlock-auth: $PC_JWT")
 
+quick_check "/policy"
 
-POLICY_JSON=$(curl --request GET \
-     --url "$PC_APIURL/policy?" \
-     --header "x-redlock-auth: $PC_JWT" | jq -r '[.[] | {name: .name, description: .description, cloudtype: .cloudtype, complianceMetadata: .complianceMetadata[]?} | {name: .name, description: .description, cloudtype: .cloudtype, standardName: .complianceMetadata.standardName, standardDescription: .complianceMetadata.standardDescription, requirementId: .complianceMetadata.requirementId, requirementName: .complianceMetadata.requirementName, sectionId: .complianceMetadata.sectionId, sectionDescription: .complianceMetadata.sectionDescription, sectionViewOrder: .complianceMetadata.sectionViewOrder, requirementViewOrder: .complianceMetadata.requirementViewOrder }]'  | jq -r --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '[.[] | select(.standardName == $COMPLIANCE_NAME)]')
+POLICY_JSON=$(printf %s "$POLICY_JSON_RESPONSE" | jq -r '[.[] | {name: .name, description: .description, cloudtype: .cloudtype, complianceMetadata: .complianceMetadata[]?} | {name: .name, description: .description, cloudtype: .cloudtype, standardName: .complianceMetadata.standardName, standardDescription: .complianceMetadata.standardDescription, requirementId: .complianceMetadata.requirementId, requirementName: .complianceMetadata.requirementName, sectionId: .complianceMetadata.sectionId, sectionDescription: .complianceMetadata.sectionDescription, sectionViewOrder: .complianceMetadata.sectionViewOrder, requirementViewOrder: .complianceMetadata.requirementViewOrder }]'  | jq -r --arg COMPLIANCE_NAME "$COMPLIANCE_NAME" '[.[] | select(.standardName == $COMPLIANCE_NAME)]')
 
 printf %s $POLICY_JSON | jq -r 'map({name, description, cloudtype, standardName, standardDescription, requirementId, requirementName, sectionId, sectionDescription, sectionViewOrder, requirementViewORder}) | (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows | $keys,$rows[] | @csv' > $POLICY_REPORT_NAME
 

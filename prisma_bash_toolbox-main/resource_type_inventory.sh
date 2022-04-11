ddd83fc029a2304bdccedb88973e5370aa661371
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-04-11 15:01:49 -0400

    Update resource_type_inventory.sh

--- prisma_bash_toolbox-main/resource_type_inventory.sh
+++ prisma_bash_toolbox-main/resource_type_inventory.sh
@@ -22,7 +22,7 @@ TIMEAMOUNT="3" # integer value
 ####### No edits needed below this line
 
 
-function quick_check {
+quick_check () {
   res=$?
   if [ $res -eq 0 ]; then
     echo "$1 request succeeded"

89e9056fd0acbbdbf32c5d2ba7d505b6258c50eb
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-04-14 15:09:21 -0400

    Create enterprise_edition_cspm_api_token.sh

---
+++ prisma_bash_toolbox-main/enterprise_edition_cspm_api_token.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Requires jq to be installed
+# Author Kyle Butler
+
+
+source ./secrets/secrets
+
+quick_check () {
+  res=$?
+  if [ $res -eq 0 ]; then
+    echo "$1 request succeeded"
+  else
+    echo "$1 request failed error code: $res" >&2
+    exit 1
+  fi
+}
+
+AUTH_PAYLOAD=$(cat <<EOF
+{"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
+EOF
+)
+
+
+PC_JWT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/login" \
+                       --header 'Accept: application/json; charset=UTF-8' \
+                       --header 'Content-Type: application/json; charset=UTF-8' \
+                       --data "${AUTH_PAYLOAD}")
+
+quick_check "/login"
+
+
+#create some space
+echo
+echo
+echo "API token is:"
+
+printf %s "$PC_JWT_RESPONSE" | jq -r '.token' 

529832ec1d7084ecea4226cbd2a130107e60838e
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-05-31 11:57:09 -0400

    Update ci_retrieve_twistcli.sh

--- prisma_bash_toolbox-main/ci_retrieve_twistcli.sh
+++ prisma_bash_toolbox-main/ci_retrieve_twistcli.sh
@@ -4,19 +4,12 @@
 # downloads the twistcli tool from the Prisma Cloud Compute side of the console. 
 
 source ./secrets/secrets
+source ./func/func.sh
 
 
-quick_check () {
-  res=$?
-  if [ $res -eq 0 ]; then
-    echo "$1 request succeeded"
-  else
-    echo "$1 request failed error code: $res" >&2
-    exit 1
-  fi
-}
+tl-var-check
 
-wget --header "Authorization: Basic $(echo -n $PC_ACCESSKEY:$PC_SECRETKEY | base64 | tr -d '\n')" "$TL_CONSOLE/api/v1/util/twistcli"
+wget --header "Authorization: Basic $(echo -n $TL_USER:$TL_PASSWORD | base64 | tr -d '\n')" "$TL_CONSOLE/api/v1/util/twistcli"
 
 quick_check "/api/v1/util/twistcli"
 

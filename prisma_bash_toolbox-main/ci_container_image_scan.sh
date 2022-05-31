4aca090ade1961ed40b6fe76ac40740d5c425d2a
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-05-31 11:42:29 -0400

    Update ci_container_image_scan.sh

--- prisma_bash_toolbox-main/ci_container_image_scan.sh
+++ prisma_bash_toolbox-main/ci_container_image_scan.sh
@@ -6,9 +6,13 @@
 # Add twistcli to runner prior to executing script
 
 source ./secrets/secrets
+source ./func/func.sh
+
 # change to correct tag
+
 IMAGE_TAG="python:latest"
 
+tl-var-check
 # ./twistcli images scan --podman or --podman-path PATH depending on install --address $TL_CONSOLE -u $TL_USER -p $TL_PASSWORD --details $IMAGE_TAG
 ./twistcli images scan --address $TL_CONSOLE -u $TL_USER -p $TL_PASSWORD --details $IMAGE_TAG    
 

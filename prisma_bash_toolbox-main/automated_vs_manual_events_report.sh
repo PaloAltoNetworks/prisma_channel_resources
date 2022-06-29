11617883e6cb62c2ed27be3835f6d7d1a2f32c16
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-06-29 13:52:42 -0400

    Create automated_vs_manual_events_report.sh

---
+++ prisma_bash_toolbox-main/automated_vs_manual_events_report.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Written by Kyle Butler
+# Shows how many events are performed by a user vs automation task
+
+# choose aws, azure, gcp, oci....capitilization matters
+CLOUD_TYPE="aws"
+# choose hour, day, month, year
+TIME_UNIT="hour"
+# choose integer amount
+TIME_AMOUNT="24"
+
+source ./secrets/secrets
+source ./func/func.sh
+
+
+AUTH_PAYLOAD=$(cat <<EOF
+{"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
+EOF
+)
+
+
+PC_JWT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/login" \
+                       --header 'Accept: application/json; charset=UTF-8' \
+                       --header 'Content-Type: application/json; charset=UTF-8' \
+                       --data "${AUTH_PAYLOAD}")
+
+quick_check "/login"
+
+
+PC_JWT=$(printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
+
+EVENT_SEARCH=$(cat <<EOF
+{
+  "query":"event from cloud.audit_logs where cloud.type = '$CLOUD_TYPE'",
+  "timeRange":{
+     "type":"relative",
+     "value":{
+        "unit":"$TIME_UNIT",
+        "amount":$TIME_AMOUNT
+     }
+  }
+}
+EOF
+)
+
+EVENT_RESPONSE=$(curl --request POST \
+                       --url "$PC_APIURL/search/event" \
+                       --header 'content-type: application/json; charset=UTF-8' \
+                       --header "x-redlock-auth: $PC_JWT" \
+                       --data "$EVENT_SEARCH")
+
+
+NUMBER_OF_EVENTS=$(printf '%s' "$EVENT_RESPONSE" | jq '.data.items[] | .id' | wc -l)
+NUMBER_OF_MANUAL_EVENTS=$(printf '%s' "$EVENT_RESPONSE" | jq '.data.items[]| select(.ip != null) | .ip ' | wc -l)
+NUMBER_OF_AUTOMATED_EVENTS=$(printf '%s' "$EVENT_RESPONSE" | jq '.data.items[]| select(.ip == null) | .ip ' | wc -l)
+
+METRICS_JSON=$(cat <<EOF
+{
+ "cloudType": "$CLOUD_TYPE",
+ "reportTimeUnit": "$TIME_UNIT",
+ "reportTimeAmount": "$TIME_AMOUNT",
+ "manualEvents": "$NUMBER_OF_MANUAL_EVENTS",
+ "automatedEvents": "$NUMBER_OF_AUTOMATED_EVENTS",
+ "totalEvents": "$NUMBER_OF_EVENTS"
+}
+EOF
+)
+
+
+printf '%s' "$METRICS_JSON" | jq

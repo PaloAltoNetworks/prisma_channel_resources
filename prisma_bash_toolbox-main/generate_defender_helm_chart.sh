d37dc57f14cf221858f77a3d71930fd9e8e3aaf9
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2023-06-23 17:26:00 -0400

    Update generate_defender_helm_chart.sh

--- prisma_bash_toolbox-main/generate_defender_helm_chart.sh
+++ prisma_bash_toolbox-main/generate_defender_helm_chart.sh
@@ -13,7 +13,7 @@ EOF
 
 
 
-HOSTNAME_FOR_CONSOLE=$(printf %s $TL_CONSOLE | awk -F / '{print $3}' | sed  s/':\S*'//g)
+HOSTNAME_FOR_CONSOLE=$(printf %s $TL_CONSOLE | awk -F / '{print $3}' | sed  's/':\S*'//g')
 
 
 # authenticates to the prisma compute console using the access key and secret key. If using a self-signed cert with a compute on-prem version, add -k to the curl command.·

ffc9876fa69315c6c3fd092a191f2d4af226680b
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2023-07-24 13:35:21 -0400

    Update func.sh
    fixed the logic to handle MacOS

--- prisma_bash_toolbox-main/func/func.sh
+++ prisma_bash_toolbox-main/func/func.sh
@@ -3,7 +3,7 @@
 # use to validate the prisma variable assignements in ./secrets/secrets
 
 
-
+OS_CHECK=$(uname -s)
 PC_SECRETKEY_MATCH='^(\w|\d|\/|\+){27}\=$'
 PC_ACCESSKEY_MATCH='^(\d|\w){8}\-(\d|\w){4}\-(\d|\w){4}\-(\d|\w){4}\-(\d|\w){12}$'
 PC_APIURL_MATCH='^https\:\/\/api[2-9]?\.(anz|eu|gov|ca|sg|uk|ind)?(\.)?prismacloud\.(cn|io)$'
@@ -11,62 +11,112 @@ TL_CONSOLE_MATCH='^https\:\/\/(\w|\d|\.|\-|\_|\:|\/)+$'
 
 # function to check variables required to access the prisma cloud enterprise api endpoints for cspm
 pce-var-check () {
-if [[ ! $PC_SECRETKEY =~ $PC_SECRETKEY_MATCH ]]
-  then
-     printf '\n%s' "PC_SECRETKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
-     read -r CONTINUE
-     if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]
-       then
-         printf '\n%s' "running script..."
-     else
-        printf '\n%s' "try running the setup.sh script"
-        exit 1
-     fi
+if [[ "$OS_CHECK" == "Darwin" ]]; then \
+  ACCESSKEY_CHECK=$(printf '%s' "$PC_ACCESSKEY" |  grep -E "$PC_ACCESSKEY_MATCH")
+  if [ -z "$ACCESSKEY_CHECK" ]; then \
+    printf '\n%s' "PC_ACCESSKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
+else
+  ACCESSKEY_CHECK=$(printf '%s' "$PC_ACCESSKEY" |  grep -P "$PC_ACCESSKEY_MATCH")
+  if [ -z "$ACCESSKEY_CHECK" ]; then \
+    printf '\n%s' "PC_ACCESSKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
 fi
 
-if [[ ! $PC_ACCESSKEY =~ $PC_ACCESSKEY_MATCH ]]
-  then
-     printf '\n%s' "PC_ACCESSKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
-     read -r CONTINUE
-     if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]
-       then
-         printf '\n%s' "running script..."
-     else
-        printf '\n%s' "try running the setup.sh script"
-        exit 1
-     fi
+if [[ "$OS_CHECK" == "Darwin" ]]; then \
+  SECRETKEY_CHECK=$(printf '%s' "$PC_SECRETKEY" |  grep -E "$PC_SECRETKEY_MATCH")
+  if [ -z "$ACCESSKEY_CHECK" ]; then \
+    printf '\n%s' "PC_SECRETKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
+else
+  SECRETKEY_CHECK=$(printf '%s' "$PC_SECRETKEY" |  grep -P "$PC_SECRETKEY_MATCH")
+  if [ -z "$ACCESSKEY_CHECK" ]; then \
+    printf '\n%s' "PC_SECRETKEY does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
 fi
 
-
-if [[ ! $PC_APIURL =~ $PC_APIURL_MATCH ]]
-  then
-     printf '\n%s' "The Prisma Cloud api url does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
-     read -r CONTINUE
-     if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]
-       then
-         printf '\n%s' "running script..."
-     else
-        printf '\n%s' "try running the setup.sh script"
-        exit 1
-     fi
+if [[ "$OS_CHECK" == "Darwin" ]]; then \
+  PC_APIURL_CHECK=$(printf '%s' "$PC_APIURL" |  grep -E "$PC_APIURL_MATCH")
+  if [ -z "$PC_APIURL_CHECK" ]; then \
+    printf '\n%s' "PC_APIURL does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
+else
+  PC_APIURL_CHECK=$(printf '%s' "$PC_APIURL" |  grep -P "$PC_APIURL_MATCH")
+  if [ -z "$PC_APIURL_CHECK" ]; then \
+    printf '\n%s' "PC_APIURL does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
 fi
 }
 
 # function to check variables required to access the compute api endpoints
 tl-var-check () {
-if [[ ! $TL_CONSOLE =~ $TL_CONSOLE_MATCH ]]
-  then
-     printf '\n%s' "Prisma Compute api url does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
-     read -r CONTINUE
-     if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]
-       then
-         printf '\n%s' "running script..."
-     else
-        printf '\n%s' "try running the setup.sh script"
-        exit 1
-     fi
+if [[ "$OS_CHECK" == "Darwin" ]]; then \
+  TL_CONSOLE_CHECK=$(printf '%s' "$TL_CONSOLE" |  grep -E "$TL_CONSOLE_MATCH")
+  if [ -z "$TL_CONSOLE_CHECK" ]; then \
+    printf '\n%s' "TL_CONSOLE does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
+else
+  TL_CONSOLE_CHECK=$(printf '%s' "$TL_CONSOLE" |  grep -P "$TL_CONSOLE_MATCH")
+  if [ -z "$ACCESSKEY_CHECK" ]; then \
+    printf '\n%s' "TL_CONSOLE does not meet the regex validation check in the ./secrets/secrets file. Would you like to continue?"
+    read -r CONTINUE
+    if [ "$CONTINUE" != "${CONTINUE#[Yy]}" ]; then \
+      printf '\n%s' "running script..."
+    else
+      printf '\n%s' "try running the setup.sh script"
+      exit 1
+    fi
+  fi
 fi
-
 if [ -z "$TL_USER" ]
   then
     echo "TL_USER variable is unassigned. Run the setup.sh script or fix the variable assignment in the secrets directory"
